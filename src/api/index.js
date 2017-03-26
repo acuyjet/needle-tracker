71e2088921af4780a243719778e62aa7508499dd
acuyjet <allison@cuyjet.com>
2017-03-25 20:53:03 -0400

    Add mock data for activity entries

--- src/api/index.js
+++ src/api/index.js
@@ -1,16 +1,17 @@
 'use strict';
 
 var express = require('express');
+var entries = require('../../mock/entries.json'); // Mock data
 var router = express.Router();
 
 // GET route for activity entries
 router.get('/entries', function(req, res) {
-	res.json({entries:[]});
+	res.json({entries:entries});
 });
 
 // GET route for leaderboard
 router.get('/leaderboard', function(req, res) {
-	res.json({leaderboard:[]});
+	res.send('Leaderboard goes here');
 });
 
 // TODO: Add POST route for new entries
