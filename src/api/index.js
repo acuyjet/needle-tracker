15dcbfd54d02f32353cd556847b20a8f102cd19e
acuyjet <allison@cuyjet.com>
2017-03-26 21:30:36 -0400

    Replace mock data with Activity model in GET route

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,20 @@
 'use strict';
 
 var express = require('express');
-var entries = require('../../mock/entries.json'); // Mock data
+var Activity = require('../models/activity');
+//var entries = require('../../mock/activities.json'); // Mock data
+
 var router = express.Router();
 
 // GET route for activity entries
-router.get('/entries', function(req, res) {
-	res.json({entries:entries});
+router.get('/activities', function(req, res) {
+	Activity.find({}, function(err, activities){
+		if(err) {
+			res.status(500).json({ message: err.message });
+		}
+		res.json({activities:activities});
+	});
+
 });
 
 // GET route for leaderboard
@@ -14,10 +22,10 @@ router.get('/leaderboard', function(req, res) {
 	res.send('Leaderboard goes here');
 });
 
-// TODO: Add POST route for new entries
+// TODO: Add POST route for new activities
 
-// TODO: Add PUT route to update entries
+// TODO: Add PUT route to update activities
 
-// TODO: Add DELETE route to delete entries
+// TODO: Add DELETE route to delete activities
 
 module.exports = router;
