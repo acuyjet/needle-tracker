91c2fc6aa317e181ea97e03757d4dd31ee4fb132
acuyjet <allison@cuyjet.com>
2017-03-29 20:11:10 -0400

    Stub out save, edit, and delete functions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,21 @@
 'use strict';
 
 // Create Angular app
-angular.module("needle-tracker", []);
+angular.module("needle-tracker", [])
+		.controller('mainCtrl', function($scope) {
+			$scope.saveForm = function() {
+				console.log('Form saved!');
+			};
+
+			$scope.needleList = function() {
+				console.log('A list of needles!');
+			};
+
+			$scope.editNeedle = function() {
+				console.log('Editing a needle!')
+			};
+
+			$scope.deleteNeedle = function() {
+				console.log('Needle deleted!');
+			}
+		});
