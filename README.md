df90044ceff21ac9953568d15006b9bf9df1acbe
acuyjet <allison@cuyjet.com>
2017-03-30 19:56:27 -0400

    Update README; make Angular changes

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # needle-tracker
 A place to keep track of all the knitting needles you've got scattered around your house so you can finally get down 
 to business and finish the hat you promised your sister back in February. 
+
+
+##### Issues to address:
+* Change text box inputs back to checkboxes and figure out how to get them to work well with Angular
+* UI improvements
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,6 +3,15 @@
 // Create Angular app
 angular.module("needle-tracker", [])
 		.controller('mainCtrl', function($scope) {
+
+			// $scope.sizes = [ 'US 00', 'US 0', 'US 1', 'US 1 1/2', 'US 2', 'US 2 1/2', 'US 3', 'US 4', 'US 5', 'US 6', 'US 7', 'US 8', 'US 9', 'US 10', 'US 10 1/2', 'US 11', 'US 13', 'US 15', 'US 17', 'US 19', 'US 35', 'US 50' ];
+
+			$scope.needles = [
+				{
+
+				}
+			];
+
 			$scope.saveForm = function() {
 				console.log('Form saved!');
 			};
@@ -17,5 +26,5 @@ angular.module("needle-tracker", [])
 
 			$scope.deleteNeedle = function() {
 				console.log('Needle deleted!');
-			}
+			};
 		});
