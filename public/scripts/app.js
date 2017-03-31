feb9ae752f5435ea147d5ab89a6dcff9a34c5dab
acuyjet <allison@cuyjet.com>
2017-03-30 22:58:18 -0400

    Stub out functions to save and delete needles

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,37 +4,56 @@
 angular.module("needle-tracker", [])
 		.controller('mainCtrl', function($scope, dataService) {
 
-			$scope.addNeedle = function() {
-				console.log('Needle added to list!');
-			};
+			// $scope.addNeedle = function() {
+			// 	console.log('Needle added to list!');
+			// };
 
-			$scope.getNeedles = function() {
-				console.log('A list of needles!');
-			};
+			// $scope.getNeedles = function() {
+			// 	console.log('A list of needles!');
+			// };
 
-			$scope.editNeedle = function() {
-				console.log('Edited a needle!')
-			};
+			// $scope.editNeedle = function() {
+			// 	console.log('Edited a needle!')
+			// };
 
-			$scope.deleteNeedle = function() {
-				console.log('Needle deleted!');
-			};
+			// $scope.deleteNeedle = function() {
+			// 	console.log('Needle deleted!');
+			// };
 
+			// Get list of needles
 			dataService.getNeedles(function(response) {
 				console.log(response.data);
 				$scope.needles = response.data;
 			});
+
+			// Save a needle
+			$scope.saveNeedle = function(needle) {
+				dataService.saveNeedle(needle);
+			};
+
+			// Delete needle and remove from list
+			$scope.deleteNeedle = function(needle, $index) {
+				dataService.deleteNeedle(needle);
+				$scope.needles.splice($index, 1);
+			};
 		})
 
 		.service('dataService', function($http) {
-			// Add a needle
-			this.addNeedle = function() {
-				console.log('Hello, world!');
-			};
-
 			// Get list of needles
 			this.getNeedles = function(callback) {
 				$http.get('mock/needles.json')
 						.then(callback)
+			};
+
+			// Save a needle
+			this.saveNeedle = function(needle) {
+				console.log(needle + ' has been saved!');
+				// DB logic goes here
+			};
+
+			// Delete a needle
+			this.deleteNeedle = function(needle) {
+				console.log(needle.size + '/' + needle.type + '/' + needle.material + ' has been deleted!');
+				// DB logic goes here
 			};
 		});
